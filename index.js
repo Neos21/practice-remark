6c38d30401b72fca703ebeac724cbef1964351b4
Neos21 <neos21@gmail.com>
2020-10-21 13:17:17 +0900

    Inspect

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
 const fs = require('fs').promises;
 
-const unified         = require('unified');
-const remarkParse     = require('remark-parse');
-const remarkRehype    = require('remark-rehype');
-const rehypeSlug      = require('rehype-slug');
-const rehypeToc       = require('@jsdevtools/rehype-toc');
-const rehypeStringify = require('rehype-stringify');
-const rehypeFormat    = require('rehype-format');
+const unified          = require('unified');
+const remarkParse      = require('remark-parse');
+const remarkRehype     = require('remark-rehype');
+const rehypeSlug       = require('rehype-slug');
+const rehypeToc        = require('@jsdevtools/rehype-toc');
+const rehypeStringify  = require('rehype-stringify');
+const rehypeFormat     = require('rehype-format');
+const unistUtilInspect = require('unist-util-inspect');
 
 (async () => {
   try {
-    await main();
+    const inputMarkdownText = await fs.readFile('./example.md', 'utf-8');
+    inspectMarkdownToHtml(inputMarkdownText);
+    const outputHtmlCode = await markdownToHtml(inputMarkdownText);
+    console.log(outputHtmlCode);
   }
   catch(error) {
     console.error('ERROR :\n', error, '\nERROR');
   }
 })();
 
-async function main() {
+function inspectMarkdownToHtml(inputMarkdownText) {
+  // processor.process() = parse() And run() And stringify()
   const processor = unified()
-    .use(remarkParse)       // Markdown To mdast (Markdown AST)
+    .use(remarkParse)       // Markdown To mdast (Markdown AST) https://unifiedjs.com/explore/package/remark-parse/
     .use(remarkRehype)      // mdast To hast (HTML AST)
-    .use(rehypeSlug)        // Add Slug (Headline ID) In hast
-    .use(rehypeToc)         // Add Table of Contents : Options https://github.com/JS-DevTools/rehype-toc#usage
-    .use(rehypeStringify)   // hast To HTML
-    .use(rehypeFormat);     // HTML Format
+    .use(rehypeStringify);  // hast To HTML
   
-  const input = await fs.readFile('./index.md', 'utf-8');
+  const mdast = processor.parse(inputMarkdownText);
+  console.log('mdast :\n', unistUtilInspect(mdast), '\nmdast\n');
+  //console.log('Raw mdast :\n', mdast, '\nRaw mdast\n');
   
-  const result = await processor.process(input);
-  const contents = result.contents;  // HTML Code
-  console.log(contents);
+  const hast = processor.runSync(mdast);
+  //console.log('hast :\n', unistUtilInspect(hast), '\nhast\n');
+  //console.log('Raw hast :\n', hast, '\nRaw hast\n');
+}
+
+async function markdownToHtml(inputMarkdownText) {
+  const processor = unified()
+    .use(remarkParse)      // Markdown To mdast (Markdown AST)
+    .use(remarkRehype)     // mdast To hast (HTML AST)
+    .use(rehypeSlug)       // Add Slug (Headline ID) In hast
+    .use(rehypeToc)        // Add Table of Contents : Options https://github.com/JS-DevTools/rehype-toc#usage
+    .use(rehypeStringify)  // hast To HTML
+    .use(rehypeFormat);    // HTML Format
+  const result = await processor.process(inputMarkdownText);
+  return result.contents;
 }
 
 /*
-
-# Ref
 
 - https://github.com/remarkjs/awesome-remark
 - Unuse
@@ -48,6 +62,8 @@ async function main() {
   - GitHub Flavored Markdown
 - remark-lint : https://github.com/remarkjs/remark-lint
   - Markdown Lint
+- remark-hint : https://github.com/sergioramos/remark-hint
+  - New Syntax
 
 - https://github.com/rehypejs/awesome-rehype
 - rehype-parse : https://github.com/rehypejs/rehype/tree/main/packages/rehype-parse
