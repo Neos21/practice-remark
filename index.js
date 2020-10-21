a143516543ba6dd164d83ebe23be55d2ea073c7e
Neos21 <neos21@gmail.com>
2020-10-21 11:24:25 +0900

    見出しに ID 付与

--- index.js
+++ index.js
@@ -3,19 +3,10 @@ const fs = require('fs').promises;
 const unified         = require('unified');
 const remarkParse     = require('remark-parse');
 const remarkRehype    = require('remark-rehype');
+const rehypeSlug      = require('rehype-slug');
+const rehypeToc       = require('@jsdevtools/rehype-toc');
 const rehypeStringify = require('rehype-stringify');
 const rehypeFormat    = require('rehype-format');
-
-// rehype-parse     : HTML To hast
-// rehype-remark    : hast To mdast
-// remark-stringify : mdast To Markdown
-
-// remark-toc : mdast -> Table of Contents
-// remark-html
-// remark-lint
-// remark-preset-lint-markdown-style-guide
-// unified-stream : process.stdin.pipe(unifiedStream(processor)).pipe(process.stdout);
-// rehype-document
 
 (async () => {
   try {
@@ -30,6 +21,8 @@ async function main() {
   const processor = unified()
     .use(remarkParse)       // Markdown To mdast (Markdown AST)
     .use(remarkRehype)      // mdast To hast (HTML AST)
+    .use(rehypeSlug)        // Add Slug (Headline ID) In hast
+    .use(rehypeToc)         // Add Table of Contents : Options https://github.com/JS-DevTools/rehype-toc#usage
     .use(rehypeStringify)   // hast To HTML
     .use(rehypeFormat);     // HTML Format
   
@@ -44,7 +37,39 @@ async function main() {
 
 # Ref
 
+- https://github.com/remarkjs/awesome-remark
+- Unuse
+  - remark-html : remark-rehype And rehype-stringify
+  - remark-slug : https://github.com/remarkjs/remark-slug
+  - remark-autolink-headings : https://github.com/remarkjs/remark-autolink-headings
+- remark-stringify : https://github.com/remarkjs/remark/tree/HEAD/packages/remark-stringify
+  - mdast To Markdown
+- remark-gfm : https://github.com/remarkjs/remark-gfm
+  - GitHub Flavored Markdown
+- remark-lint : https://github.com/remarkjs/remark-lint
+  - Markdown Lint
+
+- https://github.com/rehypejs/awesome-rehype
+- rehype-parse : https://github.com/rehypejs/rehype/tree/main/packages/rehype-parse
+  - HTML To hast
+- rehype-remark : https://github.com/rehypejs/rehype-remark
+  - hast To mdast
 - @agentofuser/rehype-section : https://github.com/agentofuser/rehype-section
   - Wrap `section`
+- @mapboxrehype-prism : https://github.com/mapbox/rehype-prism
+  - Prism.js
+- rehype-highlight : https://github.com/rehypejs/rehype-highlight
+  - Highlight.js
+- rehype-document : https://github.com/rehypejs/rehype-document
+  - Add `html`・`head`・`body`
+- rehype-meta : https://github.com/rehypejs/rehype-meta
+  - Add `meta`
+- rehype-preset-minify : https://github.com/rehypejs/rehype-minify
+  - HTML Minify
+- rehype-autolink-headings : https://github.com/rehypejs/rehype-autolink-headings
+  - `<h1 id="lorem"><a href="#lorem">Icon</a>Lorem</h1>`
+
+- unified-stream : https://github.com/unifiedjs/unified-stream
+  - `process.stdin.pipe(unifiedStream(processor)).pipe(process.stdout);`
 
  */
